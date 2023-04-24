18d84d2543b267a90445d9cc1965d3c83c5bec37
LiBrisk <kseniavolga3421@gmail.com>
2023-04-24 17:58:18 +0200

    Add Films Manager

---
+++ src/test/java/FilmsManagerTest.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmsManagerTest {
+    @Test
+    public void shouldAddThreeMovie() {
+        FilmsManager manager = new FilmsManager();
+        manager.addFilm("Movie1");
+        manager.addFilm("Movie2");
+        manager.addFilm("Movie3");
+        String[] actual = manager.findAll();
+        String[] expected = {"Movie1","Movie2", "Movie3"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldAddOneMovie() {
+        FilmsManager manager = new FilmsManager();
+        manager.addFilm("Movie1");
+        String[] actual = manager.findAll();
+        String[] expected = {"Movie1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldAddZeroMovie() {
+        FilmsManager manager = new FilmsManager();
+        String[] actual = manager.findAll();
+        String[] expected = { };
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldAddThreeMoviePrev() {
+        FilmsManager manager = new FilmsManager();
+        manager.addFilm("Movie1");
+        manager.addFilm("Movie2");
+        manager.addFilm("Movie3");
+        String[] actual = manager.findLast();
+        String[] expected = {"Movie3","Movie2", "Movie1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldAddFiveMoviePrev() {
+        FilmsManager manager = new FilmsManager();
+        manager.addFilm("Movie1");
+        manager.addFilm("Movie2");
+        manager.addFilm("Movie3");
+        manager.addFilm("Movie4");
+        manager.addFilm("Movie5");
+        String[] actual = manager.findLast();
+        String[] expected = {"Movie5", "Movie4", "Movie3","Movie2", "Movie1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+//   @Test
+//    public void shouldAddThreeFromFiveMoviePrev() {
+//        FilmsManager manager = new FilmsManager(3);
+//        manager.addFilm("Movie1");
+//        manager.addFilm("Movie2");
+//        manager.addFilm("Movie3");
+//        manager.addFilm("Movie4");
+//        manager.addFilm("Movie5");
+//        String[] actual = manager.findLast();
+//        String[] expected = {"Movie5", "Movie4", "Movie3"};
+//        Assertions.assertArrayEquals(expected, actual);
+//    }
+    @Test
+    public void shouldAddZeroMoviePrev() {
+        FilmsManager manager = new FilmsManager();
+        String[] actual = manager.findLast();
+        String[] expected = { };
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
